b5ed072066c5f7394ca9f05abbd678a514009bc3
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-10-18 18:21:14 -0400

    Update cafes.py

--- main/cafes.py
+++ main/cafes.py
@@ -4,10 +4,10 @@ from PIL import Image
 import base64
 import io
 import datetime
-import pywhatkit
 import os
 
 os.environ['DISPLAY'] = ':0'
+import pywhatkit
 
 st.set_page_config(layout="wide", page_title="Coffees", page_icon="./img/cafe5.png")
 
